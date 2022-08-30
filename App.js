ebb89c0f5aa71f63975f765ba09c7cf06d728be0
Roman Ostash <romeoostashko@gmail.com>
2022-08-29 22:02:20 -0500

    init, install vector icons

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const App = () => {
+  return <SafeAreaView></SafeAreaView>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
